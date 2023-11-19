895ee008f03799adca6d773204f4902e45b878f6
deepgohil <monisha.g@somaiya.edu>
2023-11-19 14:48:24 +0000

    prod code updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -32,3 +32,77 @@ window.addEventListener("scroll", function () {
   }
 
 });
+
+
+
+document.getElementById('getInTouchBtn').addEventListener('click', function() {
+    // Redirect to form.html
+    window.location.href = 'form.html';
+});
+document.getElementById('getInTouchBtn1').addEventListener('click', function() {
+    // Redirect to form.html
+    window.location.href = 'form.html';
+});
+document.getElementById('getInTouchBtn2').addEventListener('click', function() {
+    // Redirect to form.html
+    window.location.href = 'form.html';
+});
+
+ function submitForm() {
+            // Get the email input value
+            var email = document.getElementById('email').value;
+
+            // Create a JSON object with the email
+            var data = {
+                email: email
+            };
+
+            // Make a POST request to your serverless function
+            fetch('https://y0546fywr0.execute-api.ap-south-1.amazonaws.com/sendmail', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log('Success:', data);
+                // Handle the response as needed
+            })
+            .catch(error => {
+                console.error('Error:', error);
+                // Handle errors
+            });
+        }
+
+
+const contactForm = document.getElementById('contactForm');
+        
+        contactForm.addEventListener('submit', function (e) {
+            e.preventDefault();
+
+            const formData = new FormData(contactForm);
+            const formDataObject = {};
+
+            formData.forEach((value, key) => {
+                formDataObject[key] = value;
+            });
+
+            // Make an API request using fetch
+            fetch('https://y0546fywr0.execute-api.ap-south-1.amazonaws.com/sendmail', {
+                mode: 'no-cors',
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(formDataObject),
+            })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data); 
+            })
+            .catch(error => {
+                console.error('Error:', error);
+            });
+        });
